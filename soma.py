603990f09bd936906fc5ae6484f9cf56cf7c0188
profjocile <168863752+profjocile@users.noreply.github.com>
2024-05-10 15:52:45 -0300

    primeira versao da soma

---
+++ soma.py
@@ -0,0 +1,7 @@
+# Faça um algoritmo para ler dois números
+# e imprimir a soma dos números lidos.
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+soma = n1 + n2
+print(f'A soma dos números {n1} + {n2} é {soma}')
